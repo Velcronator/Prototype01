ed6f51104cb7d4a69c523a21e52d6494af6587fb
velcronator <valacos7@gmail.com>
2024-08-11 23:24:36 +1000

    Plane

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float cameraSpeed = 5.0f;
+    [SerializeField] private float turnSpeed = 5.0f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+
+    }
+}
